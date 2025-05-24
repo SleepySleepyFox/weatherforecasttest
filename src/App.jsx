2bc206e613a420bdf6b91129af562f2909b49413
@Yevhen.Petrikeiev <ShkrebNsh@gmail.com>
2025-05-24 20:44:32 +0300

    feat: create basic input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Search from "./components/search/Search";
 
 function App() {
   return (
     <div className="App">
-      <div></div>
+      <Search />
     </div>
   );
 }
---
+++ src/components/search/Search.jsx
@@ -0,0 +1,9 @@
+import "./Search.css";
+export default function Search() {
+  return (
+    <form className="search-form">
+      <input />
+      <button type="submit">submit</button>
+    </form>
+  );
+}
