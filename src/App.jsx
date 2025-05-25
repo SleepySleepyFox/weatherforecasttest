dd8c043363ce33efe9f28e098afcf629c339474d
@Yevhen.Petrikeiev <ShkrebNsh@gmail.com>
2025-05-25 17:02:37 +0300

    fix: resolved non-serializable value warning in Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,19 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Chart from "./components/chart/Chart";
 import Search from "./components/search/Search";
-import axios from "axios";
 import { useEffect } from "react";
+import { fetchWeatherData } from "./store/features/ApiDataSlice";
 function App() {
   // TODO:
   // - Create charts
   // - Create fallback if city not found
   const selector = useSelector((state) => state.apiData.location);
-  const data = async (e) => {
-    if (e === null || e === undefined) return;
-    try {
-      const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/forecast?q=${e}&appid=${process.env.REACT_APP_API_KEY}`,
-      );
-      console.log(response);
-      return response;
-    } catch (err) {
-      console.error(err);
-    }
-  };
-  useEffect(() => {
-    data(selector);
-  }, [selector]);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(fetchWeatherData(selector));
+  }, [selector]);
   return (
     <div className="App">
       <Search />
--- src/store/features/ApiDataSlice.js
+++ src/store/features/ApiDataSlice.js
@@ -1,4 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchWeatherData = createAsyncThunk(
+  "weather/fetchWeather",
+  async (e) => {
+    if (!e) return;
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${e}&appid=${process.env.REACT_APP_API_KEY}`,
+      );
+      console.log(response);
+      return response.data;
+    } catch (err) {
+      console.error(err);
+    }
+  },
+);
 
 const initialState = {
   location: null,
@@ -7,6 +24,7 @@ const initialState = {
 
 export const ApiDataSlice = createSlice({
   name: "apiData",
+  status: "idle",
   initialState,
   reducers: {
     setLocation: (state, action) => {
@@ -15,6 +33,20 @@ export const ApiDataSlice = createSlice({
     setData: (state, action) => {
       state.data = action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchWeatherData.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchWeatherData.fulfilled, (state, action) => {
+        state.status = "succeded";
+        state.data = action.payload;
+      })
+      .addCase(fetchWeatherData.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
   },
 });
 export const { setData, setLocation } = ApiDataSlice.actions;
